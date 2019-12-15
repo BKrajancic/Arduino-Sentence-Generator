d1c9e53e9831b3a1c5d8a7175c8943bc6e8c00c4
Benjamin Krajancic <ben.k99@outlook.com.au>
2019-12-15 18:49:26 +1100

    main

--- qt_app/main.cpp
+++ qt_app/main.cpp
@@ -6,6 +6,17 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    /*
+    w.setStyleSheet(
+                    "QPushButton {" \
+                    "background-color: #D50000;"\
+                    "color: white;" \
+                    "padding: 0;"
+                    "border-width: 0;" \
+                    "border-radius: 6px;"\
+                    "}"
+                    );
+                   */
     w.show();
     return a.exec();
 }
--- qt_app/mainwindow.cpp
+++ qt_app/mainwindow.cpp
@@ -5,6 +5,7 @@
 #include <QString>
 #include "personal_info.hpp"
 #include "math.h"
+#include <QGraphicsDropShadowEffect>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -22,11 +23,13 @@ MainWindow::MainWindow(QWidget *parent)
     setCentralWidget(widget);
     widget->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
 
+
     btnLayout = new QGridLayout(widget);
+    btnLayout->setVerticalSpacing(32);
+    btnLayout->setHorizontalSpacing(8);
     for (auto i = 0; i < rows * cols; ++i)
     {
-        QPushButton* const button = new QPushButton(PERSONAL::URLS[i], this);
-
+        QPushButton* button = new QPushButton(PERSONAL::URLS[i], this);
         button->setFont(text_font);
         button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
         btnLayout->addWidget(button, cols + (i / rows), i % cols);
@@ -36,6 +39,18 @@ MainWindow::MainWindow(QWidget *parent)
             &QPushButton::released,
             [=]() {this->pressed(PERSONAL::URLS[i]);}
         );
+
+        QPalette button_pal = palette();
+        button_pal.setColor(QPalette::Button, QColor(139,0,0));
+        button_pal.setColor(QPalette::ButtonText, Qt::white);
+        button->setAutoFillBackground(true);
+        button->setPalette(button_pal);
+        button->setFlat(true);
+
+        QGraphicsDropShadowEffect* button_effect = new QGraphicsDropShadowEffect();
+        button_effect->setBlurRadius(3);
+        button_effect->setOffset(1, 1);
+        button->setGraphicsEffect(button_effect);
     }
 
     QT_GRAMMAR::Qt_Grammar g = QT_GRAMMAR::Qt_Grammar();
@@ -43,7 +58,20 @@ MainWindow::MainWindow(QWidget *parent)
 
 #include "../arduino/personal_grammar.hpp"
     text->set_grammar(g);
-    text->setMargin(8);
+
+    QPalette pal = palette();
+    pal.setColor(QPalette::Background, Qt::white);
+    text->setAutoFillBackground(true);
+    text->setPalette(pal);
+    text->setMargin(5);
+    text->setFrameStyle(QFrame::StyledPanel | QFrame::Plain);
+    text->setLineWidth(1);
+
+    QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect();
+    effect->setBlurRadius(12);
+    effect->setOffset(3,3);
+    //text->setGraphicsEffect(effect);
+
 
     text->shuffle();
     text->next_screen(INT_MAX);
@@ -66,10 +94,8 @@ MainWindow::~MainWindow()
 
 unsigned int MainWindow::get_width() const
 {
-    auto out = 0u;
-    for (auto i = 0; i < cols; ++i)
-    {
-        out += static_cast<unsigned int>(btnLayout->cellRect(0,i).width());
-    }
-    return out;
+    const auto last_cell = btnLayout->cellRect(1,cols - 1);
+    const auto end_x = last_cell.x() + last_cell.width();
+    const auto right_margin = btnLayout->horizontalSpacing();
+    return static_cast<unsigned int>( end_x - (2 * text->margin()) - (right_margin * 2));
 }
